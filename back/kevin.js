413a2b045471ade90f3f4699f7a79dd1a48abe9a
klemaire60 <lemaire.kevin6020@gmail.com>
2024-02-13 17:35:54 +0100

    Préparation du code

---
+++ back/kevin.js
@@ -0,0 +1,14 @@
+// kevin.js
+module.exports = function(express) {
+    const router = express.Router();
+  
+    router.get('/kevin-route', (req, res) => {
+      // Code de kevin
+
+      
+      res.send("Route de Kevin");
+    });
+  
+    return router;
+  };
+  
