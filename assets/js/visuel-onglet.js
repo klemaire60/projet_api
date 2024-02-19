fdfaf6c3ec9be4d56db5604b2d0e450c2cda9031
klemaire60 <lemaire.kevin6020@gmail.com>
2024-02-19 11:29:32 +0100

    Ajout de l'ancien code PHP

--- assets/js/visuel-onglet.js
+++
@@ -1,7 +0,0 @@
-function visuel_color() 
-{
-            var color = document.getElementById("couleur").value;
-            document.getElementById("name").style.backgroundColor=color;
-            document.getElementById("onglet_color").style.backgroundColor=color;
-}  
-
