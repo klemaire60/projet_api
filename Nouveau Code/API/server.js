956dfb3b75d30e0a58c5941347a05cac9ff5598a
klemaire60 <kekelele6020@gmail.com>
2024-03-09 00:54:18 +0100

    logique du login

--- Nouveau Code/API/server.js
+++ Nouveau Code/API/server.js
@@ -1,19 +1,56 @@
 const express = require('express');
 const app = express(); 
-const login = require('./kevin');
+const mysql = require('mysql')
+const port = 8080;
+
+const connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'journalltrpedago',
+  password : 'Pedago1234',
+  database : 'journalltrpedago'
+})
 
 connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+    throw err;
+  }
+  console.log('Connecté à la base de données MySQL');
+});
+
+// Démarrer le serveur
+app.listen(port, () => {
+  console.log(`Le serveur est en écoute sur le port ${port}`);
+});
+
+app.post('/login', (req, res) => {
+  
+  const { login, mdp } = req.body;
+  
+  if (!login || !mdp) {
+    return res.status(400).json({ message: 'login et mdp requis' });
+  }
+  
+  const sql = 'SELECT password FROM account WHERE pseudo = (?)';
+  
+  // Exécute la requête
+  connection.query(sql, [login], (err, results) => {
     if (err) {
-      console.error('Erreur de connexion à la base de données :', err);
-      throw err;
+      console.error('Erreur lors de l\'exécution de la requête :', err);
+      res.status(500).send('Erreur lors de la requête');
+      return;
     }
-    console.log('Connecté à la base de données MySQL');
+    
+    if(results == '') {
+      req.body.success = false;
+      return
+    }
+    else 
+    {
+      let passwd = sha1(results);
+      req.body.success = passwd === mdp;
+    }
+    
+    res.json(req.body);
   });
-
-//récupération des différents fichiers js
-const bastien = require('./bastien')(express);
-const yann = require('./yann')(express);
-
-app.use('./api', yann);
-app.use('./api', bastien);
-app.use('./api', login);
+});
